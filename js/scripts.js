5c8f1bef33336ca34946cf95217b6b3ee50f371a
zhong-david <dzhong@umich.edu>
2023-04-18 01:25:17 -0400

    template

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// $(document).ready(function () { 
+//     description =  $('#description');
+//     timeFrame =  $('#timeFrame');
+//     description.show();
+//     timeFrame.hide();
+
+// });
+
+
+// function showDescription() {
+//     console.log("calling 1");
+//     description.show();
+//     timeFrame.hide();
+// }
+// function showTimeFrame() {
+//     console.log("calling 2");
+
+//     timeFrame.show();
+//     description.hide();
+// }
